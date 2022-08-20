05e607508816e63b561bc917de56bf185ec81ec7
AtanuChatterjee <111640222+AtanuChatterjee@users.noreply.github.com>
2022-08-20 22:10:58 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from "react"
 
 const pageStyles = {
   color: "#232129",
-  padding: 200,
+  padding: 50,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
 const headingStyles = {
