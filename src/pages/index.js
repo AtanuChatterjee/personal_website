08ad69ae3b7b708a3a96ea03b842784961509b9b
AtanuChatterjee <111640222+AtanuChatterjee@users.noreply.github.com>
2022-08-22 23:48:39 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -87,21 +87,21 @@ const links = [
     url: "https://www.gatsbyjs.com/docs/tutorial/",
     description:
       "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-//     color: "#E95800",
+    color: "#D3D3D3",
   },
   {
     text: "Research",
     url: "https://www.gatsbyjs.com/docs/how-to/",
     description:
       "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-//     color: "#1099A8",
+    color: "#D3D3D3",
   },
   {
     text: "Contact",
     url: "https://www.gatsbyjs.com/docs/reference/",
     description:
       "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-//     color: "#BC027F",
+    color: "#D3D3D3",
   },
 //   {
 //     text: "Conceptual Guides",
