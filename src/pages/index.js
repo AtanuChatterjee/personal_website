c2658402442f5407f58c4b0d776b18f859433ef7
AtanuChatterjee <111640222+AtanuChatterjee@users.noreply.github.com>
2022-09-02 08:44:57 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -225,8 +225,8 @@ const IndexPage = () => {
         </li>
         
         <figure>
-        <img src="pic_trulli.jpg" alt="Trulli" style="width:100%">
-        <figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption>
+            <img src="pic_trulli.jpg" alt="Trulli" style="width:100%">
+            <figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption>
         </figure>
         
         {links.map(link => (
